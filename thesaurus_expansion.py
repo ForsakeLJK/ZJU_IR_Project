d0f179ca385706873f8d832afdf346b9af41f175
ForsakeLJK <forsakeljk@gmail.com>
2020-06-06 14:31:25 +0800

    getCorrMat implemented

--- thesaurus_expansion.py
+++ thesaurus_expansion.py
@@ -1,7 +1,8 @@
 import numpy as np
+import sys
 
 def getCoMat(wordList):
-    
+
     coMat = []
 
     for word in wordList:
@@ -15,3 +16,36 @@ def getCoMat(wordList):
         coMat.append(coMatEntry)
 
     return np.array(coMat)
+
+def getCorrMat(coMat):
+
+    corrMat = []
+
+    for word in coMat:
+        corrMatEntry = []
+        for w in coMat:
+            corr = corrCalc(word, w)
+            corrMatEntry.append(corr)
+        corrMat.append(corrMatEntry)
+
+    corrMat = np.array(corrMat)
+
+    return corrMat
+
+# calculate correlation between two word vectors
+def corrCalc(vec1, vec2):
+
+    if vec1.shape != vec2.shape:
+        sys.exit("In corrCalc: unequal vec shape.")
+
+    correlation = np.sum(vec1 * vec2) \
+                  / (np.sqrt(np.sum(np.square(vec1))) 
+                    * np.sqrt(np.sum(np.square(vec2))))
+
+    return correlation    
+
+def createThesarusDict(wordList):
+
+    coMat = getCoMat(wordList)
+    corrMat = getCorrMat(coMat)
+
