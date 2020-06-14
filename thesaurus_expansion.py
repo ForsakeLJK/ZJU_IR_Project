c804ad20bc0a7df6a631ede4f7ac054339d6ed48
ForsakeLJK <forsakeljk@gmail.com>
2020-06-14 23:15:43 +0800

    optimize calculation

--- thesaurus_expansion.py
+++ thesaurus_expansion.py
@@ -21,14 +21,28 @@ def getCorrMat(coMat):
 
     corrMat = []
 
+    N = coMat.shape[0]
+
+    rowCnt = 0
+    colCnt = 0
+
     for word in coMat:
         corrMatEntry = []
+        colCnt = 0
         for w in coMat:
-            corr = corrCalc(word, w)
-            corrMatEntry.append(corr)
+            if colCnt < rowCnt:
+                corr = 0
+                corrMatEntry.append(corr)
+            else:
+                corr = corrCalc(word, w)
+                corrMatEntry.append(corr)
+            colCnt += 1
         corrMat.append(corrMatEntry)
+        rowCnt += 1
 
     corrMat = np.array(corrMat)
+    
+    corrMat = np.maximum(corrMat, corrMat.transpose())
 
     return corrMat
 
@@ -36,7 +50,7 @@ def getCorrMat(coMat):
 def corrCalc(vec1, vec2):
 
     if vec1.shape != vec2.shape:
-        sys.exit("In corrCalc: unequal vec shape.")
+        sys.exit("In corrCalc: inequal vector shape.")
 
     correlation = np.sum(vec1 * vec2) \
                   / (np.sqrt(np.sum(np.square(vec1))) 
@@ -51,7 +65,7 @@ def createThesaurusDict(wordList):
     # print(corrMat)
 
     # wordCnt-by-(wordCnt-1)
-    thesarusMat = np.argsort(-corrMat, axis=1)[:, 1:]
+    thesarusMat = np.argsort(-corrMat, axis=1)
 
     return thesarusMat
 
